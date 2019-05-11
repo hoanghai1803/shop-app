b46f3fd92285c6d278defb6ad278d45dfa2bbaf2
hoanghai1803 <hoanghai123098@gmail.com>
2019-05-11 15:16:21 +0300

    Updates

---
+++ precache-manifest.7d68f7181cff3d5afba997368374d746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a9a79029610d424a500",
+    "url": "/shop-app/static/js/runtime~main.048d4945.js"
+  },
+  {
+    "revision": "7f40b6ede8e2c2fba4e0",
+    "url": "/shop-app/static/js/main.1ec51808.chunk.js"
+  },
+  {
+    "revision": "d7f68df7bc2b698c61f7",
+    "url": "/shop-app/static/js/2.d9708f12.chunk.js"
+  },
+  {
+    "revision": "d7f68df7bc2b698c61f7",
+    "url": "/shop-app/static/css/2.1c642389.chunk.css"
+  },
+  {
+    "revision": "736e33c262c36f5edfcb04a25c7ebe8f",
+    "url": "/shop-app/index.html"
+  }
+];
